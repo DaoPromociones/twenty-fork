65e79d05403b7cac0a363e980acc6d9f34a593cf
DaoPromociones <daopromociones@gmail.com>
2026-07-08 09:34:54 +0200

    test(server): cover Aduana mutation denial surface

--- packages/twenty-server/src/modules/aduana-projection/query-hooks/__tests__/aduana-projection-mutation-denial.pre-query.hook.spec.ts
+++ packages/twenty-server/src/modules/aduana-projection/query-hooks/__tests__/aduana-projection-mutation-denial.pre-query.hook.spec.ts
@@ -1,9 +1,15 @@
+import { type Module } from '@nestjs/core/injector/module';
 import { Reflector } from '@nestjs/core';
 
+import { CommonQueryNames } from 'src/engine/api/common/types/common-query-args.type';
 import { type ResolverArgs } from 'src/engine/api/graphql/workspace-resolver-builder/interfaces/workspace-resolvers-builder.interface';
+import { type WorkspacePreQueryHookInstance } from 'src/engine/api/graphql/workspace-query-runner/workspace-query-hook/interfaces/workspace-query-hook.interface';
+import { WorkspaceQueryHookStorage } from 'src/engine/api/graphql/workspace-query-runner/workspace-query-hook/storage/workspace-query-hook.storage';
 import { WorkspaceQueryHookType } from 'src/engine/api/graphql/workspace-query-runner/workspace-query-hook/types/workspace-query-hook.type';
+import { WorkspaceQueryHookExplorer } from 'src/engine/api/graphql/workspace-query-runner/workspace-query-hook/workspace-query-hook.explorer';
 import { WorkspaceQueryHookMetadataAccessor } from 'src/engine/api/graphql/workspace-query-runner/workspace-query-hook/workspace-query-hook-metadata.accessor';
 import { WorkspaceQueryHookModule } from 'src/engine/api/graphql/workspace-query-runner/workspace-query-hook/workspace-query-hook.module';
+import { WorkspaceQueryHookService } from 'src/engine/api/graphql/workspace-query-runner/workspace-query-hook/workspace-query-hook.service';
 import { type WorkspaceAuthContext } from 'src/engine/core-modules/auth/types/workspace-auth-context.type';
 import { PermissionsExceptionCode } from 'src/engine/metadata-modules/permissions/permissions.exception';
 import { ADUANA_PROJECTION_DENIED_MUTATION_HOOKS } from 'src/modules/aduana-projection/query-hooks/aduana-projection-mutation-denial.pre-query.hook';
@@ -76,4 +82,53 @@ describe('AduanaProjectionMutationDenialPreQueryHook', () => {
 
     expect(importedModules).toContain(AduanaProjectionQueryHookModule);
   });
+
+  it.each([
+    CommonQueryNames.CREATE_ONE,
+    CommonQueryNames.CREATE_MANY,
+    CommonQueryNames.UPDATE_ONE,
+    CommonQueryNames.UPDATE_MANY,
+    CommonQueryNames.DELETE_ONE,
+    CommonQueryNames.DELETE_MANY,
+    CommonQueryNames.DESTROY_ONE,
+    CommonQueryNames.DESTROY_MANY,
+    CommonQueryNames.RESTORE_ONE,
+    CommonQueryNames.RESTORE_MANY,
+    CommonQueryNames.MERGE_MANY,
+  ])(
+    'should reject Aduana projection %s through the workspace query runner pre-hook surface',
+    async (mutationMethod) => {
+      const workspaceQueryHookStorage = new WorkspaceQueryHookStorage();
+      const workspaceQueryHookExplorer = {
+        handlePreHook: async (
+          executeParams: Parameters<WorkspacePreQueryHookInstance['execute']>,
+          instance: WorkspacePreQueryHookInstance,
+        ) => instance.execute(...executeParams),
+      } as unknown as WorkspaceQueryHookExplorer;
+
+      for (const { key, Hook } of ADUANA_PROJECTION_DENIED_MUTATION_HOOKS) {
+        workspaceQueryHookStorage.registerWorkspaceQueryPreHookInstance(key, {
+          instance: new Hook(),
+          host: {} as Module,
+          isRequestScoped: false,
+        });
+      }
+
+      const workspaceQueryHookService = new WorkspaceQueryHookService(
+        workspaceQueryHookStorage,
+        workspaceQueryHookExplorer,
+      );
+
+      await expect(
+        workspaceQueryHookService.executePreQueryHooks(
+          authContext,
+          'aduanaProjection',
+          mutationMethod,
+          payload,
+        ),
+      ).rejects.toMatchObject({
+        code: PermissionsExceptionCode.METHOD_NOT_ALLOWED,
+      });
+    },
+  );
 });
